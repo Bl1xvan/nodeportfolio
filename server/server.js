232a5b263555b16ec768631d32fdb4a48b74b516
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-08-14 17:52:32 -0400

    making it better

--- server/server.js
+++ server/server.js
@@ -11,6 +11,15 @@ app.use('/api/v1/projects', projects)
 
 const port = process.env.PORT || 5000;
 
+const path = require("path");
+
+// Step 1:
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 const start = async () =>{
     try{
         await connectDB(process.env.MONGO_URI)
