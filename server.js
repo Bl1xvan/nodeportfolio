6a7d23741d9f9252f75da599b5bafa00e613ea16
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-08-18 15:28:53 -0400

    prepared for heroku deployment

--- server.js
+++ server.js
@@ -13,12 +13,10 @@ const port = process.env.PORT || 5000;
 
 const path = require("path");
 
-// Step 1:
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-// Step 2:
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
+
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+
 
 const start = async () =>{
     try{
@@ -28,6 +26,10 @@ const start = async () =>{
         console.log(error)
     }
 }
+
+app.get("*", (req, res) => {
+    response.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 
 start()
 
