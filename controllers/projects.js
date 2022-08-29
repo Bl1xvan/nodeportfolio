ba7de6327bdead44203151e2b331b2cb4959578d
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-08-29 09:31:03 -0400

    reworking projects.js

--- controllers/projects.js
+++ controllers/projects.js
@@ -2,19 +2,10 @@
 const Project = require('../models/Project')
 const asyncWrapper = require('../middleware/async')
 
-const getAllProjects = asyncWrapper(async (req, res) => {
+const getAllProjectsStatic = asyncWrapper(async (req, res) => {
 
-        const page = req.query.p || 1
-        const projectsPerPage = 4
-        const skip = page > 0 ? (page - 1) * projectsPerPage : 0
-        const count = await Project.find({}).estimatedDocumentCount();
-
-        const pageCount = Math.ceil(count / projectsPerPage)
-        const next = pageCount == page ? "null" : "not null"
-        let projects = []
-        const projectgroup = await Project.find().skip(skip).limit(projectsPerPage)
-        projectgroup.map(project => projects.push(project))
-        res.status(200).json({pageCount, next, projects})
+        const count = await Project.find({})
+        
 
 })
 
