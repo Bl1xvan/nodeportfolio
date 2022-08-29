bab2047ebdc6ac637023fc4070e6680a142af6f1
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-08-29 09:58:28 -0400

    added search filter

--- controllers/projects.js
+++ controllers/projects.js
@@ -4,12 +4,34 @@ const asyncWrapper = require('../middleware/async')
 
 const getAllProjectsStatic = asyncWrapper(async (req, res) => {
 
-        const count = await Project.find({})
-        
+        const projects = await Project.find({})
+        res.status(200).json({projects})
 
+})
+
+const getAllProjects = asyncWrapper(async(req, res) => {
+    const {title} = req.query
+    const queryObject = {}
+
+    if(title){
+        queryObject.title = {$regex: title, option: 'i'}
+    }
+
+    let result = Project.find(queryObject)
+
+    const page = Number(req.query.p) || 1
+    const limit = Number(req.query.limit) || 4
+    const skip = (page - 1) * limit
+
+    result.skip(skip).limit(limit)
+
+    const projects = await result
+    
+    res.status(200).json({projects})
 })
 
 
 module.exports = {
+    getAllProjectsStatic,
     getAllProjects
 }
