276eaed71dba90cd20afdad362cac1d4408762c1
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-09-30 15:34:45 -0400

    removed get static

--- routes/projects.js
+++ routes/projects.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {getAllProjects, getAllProjectsStatic} = require('../controllers/projects')
+const {getAllProjects} = require('../controllers/projects')
 
 router.route('/').get(getAllProjects)
-router.route('/static').get(getAllProjectsStatic)
 module.exports = router;
