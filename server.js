f3d1a988ebdc87d762d049bcbbee9009019b880d
Bl1xvan <cflavien1@my.brookdalecc.edu>
2022-10-01 17:33:35 -0400

    removed heroku components

--- server.js
+++ server.js
@@ -19,14 +19,11 @@ const port = process.env.PORT || 5000;
 
 const path = require("path");
 
-if(process.env.NODE_ENV === "production"){
 app.use(express.static(path.join(__dirname, "/client/build")));
 
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
-
-}
 
 
 const start = async () =>{
